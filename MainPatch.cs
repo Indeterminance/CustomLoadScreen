3972304ed4f22bb4f92903ced481cd9e0dadad1d
Indeterminance <75706610+Indeterminance@users.noreply.github.com>
2023-02-01 21:49:59 +0000

    BepInEx port
    Ported CLS over to BepInEx, added loading screen randomization

--- MainPatch.cs
+++ MainPatch.cs
@@ -1,18 +1,89 @@
 ﻿using System;
+using HarmonyLib;
+using BepInEx;
+using BepInEx.Logging;
+using System.IO;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Harmony;
-using System.Reflection;
+using BepInEx.Configuration;
 
 namespace CustomLoadScreen
 {
-    public static class Entry
+    [BepInPlugin(myGUID, pluginName, versionString)]
+    public class CLS_Main : BaseUnityPlugin
     {
-        public static void Patch()
+        // Mod info
+        private const string myGUID = "com.eckothebat.customloadscreen";
+        private const string pluginName = "Custom Load Screens";
+        private const string versionString = "1.3.0";
+
+        // Harmony stuff
+        public static string screens_directory = Environment.CurrentDirectory + "\\loading_screens";
+        private static readonly Harmony harmony = new Harmony(myGUID);
+        public static ManualLogSource logger = new ManualLogSource(pluginName);
+
+        // Loading screen data
+        public static System.Random rand = new System.Random();
+        public static int currentImageChoice;
+        public static List<string> images;
+        public static ConfigEntry<bool> selectRandomImages;
+        public static ConfigEntry<bool> includeVanillaScreenInRandom;
+        public static ConfigEntry<string> nonRandomImageChoice;
+
+        private void Awake()
         {
-            HarmonyInstance.Create("customload").PatchAll(Assembly.GetExecutingAssembly());
+
+            selectRandomImages = Config.Bind("General", "SelectRandomLoadingScreens", false, "Enable loading screen randomization.");
+            includeVanillaScreenInRandom = Config.Bind("General", "VanillaScreenInRandom", false, "Include the default vanilla loading screen in random selections.");
+            nonRandomImageChoice = Config.Bind("General", "NonRandomLoadingScreen", "", "Filename of loading screen to use. Only used if random loading screens are disabled. Leave blank for the vanilla loading screen.");
+            CollectLoadingScreens();
+
+            harmony.PatchAll();
+            logger = Logger;
+
+            if (!Directory.Exists(screens_directory)) Directory.CreateDirectory(screens_directory);
+        }
+
+        public void CollectLoadingScreens()
+        {
+            // Gather all files in /loading_screens, filter .png ang .jpg images, and dump to image list
+            List<string> files = new List<string>();
+            files.AddRange(Directory.GetFiles(screens_directory));
+            logger.Log(LogLevel.Info, $"Found {files.Count} files. Filtering images...");
+            images = new List<string>();
+            foreach (string file in files)
+            {
+                if (file.EndsWith(".jpg") | file.EndsWith(".png"))
+                {
+                    logger.Log(LogLevel.Debug, "Is image!!!!");
+                    images.Add(file);
+                }
+            }
+            logger.Log(LogLevel.Info, $"Found {images.Count} images:");
+            foreach (string image in images)
+            {
+                logger.Log(LogLevel.Info, image);
+            }
+
+            // Initialize our first random image, because why not
+            if (selectRandomImages.Value)
+            {
+                if (includeVanillaScreenInRandom.Value) currentImageChoice = rand.Next(-1, images.Count);
+                else currentImageChoice = rand.Next(0, images.Count);
+            }
+            else
+            {
+                currentImageChoice = ImageNameIndex(nonRandomImageChoice.Value);
+            }
+        }
+
+        public static int ImageNameIndex(string shortname)
+        {
+            char[] bs = new char[] { '\\' };
+            for (int i=0; i < images.Count; i++)
+            {
+                if (images[i].Split(bs).GetLast() == shortname) return i;
+            }
+            return -1;
         }
     }
 }
--- uGUI_SceneLoadingPatch.cs
+++ uGUI_SceneLoadingPatch.cs
@@ -1,27 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Harmony;
+﻿using HarmonyLib;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace CustomLoadScreen
 {
+    // Patch the init function of the loading screen to patch our desired graphics in
     [HarmonyPatch(typeof(uGUI_SceneLoading))]
-    [HarmonyPatch("Init")]
     public static class uGUI_SceneLoadingPatch
     {
+        [HarmonyPatch("Init")]
         public static void Postfix(uGUI_SceneLoading __instance)
         {
-            Sprite img = ImageUtils.LoadSprite(Environment.CurrentDirectory + "/QMods/CustomLoadScreen/image.png");
+            if (CLS_Main.currentImageChoice == -1) return;
+
+            Sprite img = ImageUtils.LoadSprite(CLS_Main.images[CLS_Main.currentImageChoice]);
             Image[] graphics = __instance.loadingBackground.GetComponentsInChildren<Image>();
 
-            foreach(Image graphic in graphics)
+            foreach (Image graphic in graphics)
             {
                 graphic.sprite = img;
             }
         }
+    }
+
+    // Patch the fadeout of the start screen to generate random numbers for us
+    [HarmonyPatch(typeof(StartScreenFade))]
+    public static class PressStartPatch
+    {
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        public static void Prefix()
+        {
+            if (CLS_Main.selectRandomImages.Value)
+            {
+                if (CLS_Main.includeVanillaScreenInRandom.Value) CLS_Main.currentImageChoice = CLS_Main.rand.Next(-1, CLS_Main.images.Count);
+                else CLS_Main.currentImageChoice = CLS_Main.rand.Next(0, CLS_Main.images.Count);
+            }
+            else
+            {
+                CLS_Main.currentImageChoice = CLS_Main.ImageNameIndex(CLS_Main.nonRandomImageChoice.Value);
+            }
+        }
+
     }
 }
